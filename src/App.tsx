4e4aae8abd876407192a28df3cb9f246e7e75e55
Uwe Krause <uwe@uwekrau.se>
2024-06-22 20:20:08 +0200

    add second variable

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,9 @@ SyntaxHighlighter.registerLanguage('sql', sql);
 function App() {
     const inputQueryInitial =
         "select count(*)\n" +
-        "   from table users\n" +
-        "   where name='$name';"
+        "    from table users\n" +
+        "    where name='$name'\n" +
+        "    and password='$password';"
 
     const [inputQuery, setInputQuery] =
         useState(inputQueryInitial)
@@ -19,17 +20,21 @@ function App() {
     const [inputUser, setInputUser] =
         useState("admin' or '1'='1")
 
+    const [inputPassword, setInputPassword] =
+        useState("trustno1")
+
     const [outputQuery, setOutputQuery] =
         useState(inputQueryInitial)
 
     useEffect(() => {
             setOutputQuery(
                 inputQuery
+                    .replace("$name", inputUser)
+                    .replace("$password", inputPassword)
                     .trim()
                     .replace(/\s+/g, ' ')
-                    .replace("$name", inputUser)
             )
-        }, [inputQuery, inputUser]
+        }, [inputQuery, inputUser, inputPassword]
     )
 
     return (
@@ -53,6 +58,15 @@ function App() {
                         value={inputUser}
                         onChange={(e) =>
                             setInputUser(e.target.value)}
+                    />
+                </label>
+                <br></br>
+                <label>$password
+                    <input
+                        name="password"
+                        value={inputPassword}
+                        onChange={(e) =>
+                            setInputPassword(e.target.value)}
                     />
                 </label>
             </fieldset>
