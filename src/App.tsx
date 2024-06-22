fa8e0a87d9d8c9d4eb64ba49de50d774dac6a0ff
Uwe Krause <uwe@uwekrau.se>
2024-06-22 13:31:59 +0200

    replace with input

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,69 @@
 import './App.css'
+import {useState} from "react";
 
 function App() {
+    const inputQueryInitial =
+        "select count(*) from table users\n" +
+        "    where 'name'='$name';"
+
+    const [inputQuery, setInputQuery] =
+        useState(inputQueryInitial)
+
+    const [inputUser, setInputUser] =
+        useState("user")
+
+    const [outputQuery, setOutputQuery] =
+        useState(inputQuery)
+
+    function handleInputQueryChange(targetValue: string) {
+        setInputQuery(targetValue)
+        replaceOutputWithInput(targetValue, inputUser)
+    }
+
+    function handleUsernameChange(targetValue: string) {
+        setInputUser(targetValue)
+        replaceOutputWithInput(inputQuery, targetValue)
+    }
+
+    function replaceOutputWithInput(inputQuery: string, inputName: string) {
+        setOutputQuery(
+            inputQuery
+                .replace("$name", inputName)
+        )
+    }
 
     return (
         <>
-            <h1>Vite + React</h1>
+            <fieldset>
+                <legend>Input query</legend>
+                <textarea
+                    name="inputQuery"
+                    className="query"
+                    value={inputQuery}
+                    onChange={(e) =>
+                        handleInputQueryChange(e.target.value)}
+                />
+            </fieldset>
+
+            <fieldset>
+                <legend>Input fields</legend>
+                <label>$name
+                    <input
+                        name="name"
+                        value={inputUser}
+                        onChange={(e) =>
+                            handleUsernameChange(e.target.value)}
+                    />
+                </label>
+            </fieldset>
+
+            <fieldset className="querybox">
+                <legend>Output query</legend>
+                <textarea className="query"
+                          value={outputQuery}
+                          readOnly={true}
+                />
+            </fieldset>
         </>
     )
 }
